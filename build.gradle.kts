f27804c701a5d2115d16a7053dff7a613272d88d
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-04-14 11:22:11 +0900

    0.6.0
    Sec Filter Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
     group = "com.github.Hchanjune.operation-manager-kit"
-    version = "0.5.9"
+    version = "0.6.0"
 
     repositories {
         mavenCentral()
--- spring-webmvc/src/main/kotlin/io/github/hchanjune/omk/webmvc/filter/SpringSecurityConfigurationFilter.kt
+++ spring-webmvc/src/main/kotlin/io/github/hchanjune/omk/webmvc/filter/SpringSecurityConfigurationFilter.kt
@@ -5,16 +5,21 @@ import io.github.hchanjune.omk.webmvc.Operations
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.web.filter.OncePerRequestFilter
 
 class SpringSecurityConfigurationFilter(
     private val issuerProvider: IssuerProvider
 ): OncePerRequestFilter() {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
+        logger.info((">>> IssuerInjection: ${issuerProvider.currentIssuer()}"))
         Operations.context.injectIssuer(issuerProvider.currentIssuer())
         filterChain.doFilter(request, response)
     }
