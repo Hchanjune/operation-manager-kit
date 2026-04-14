dabd12ba5fcf3d3e8b0dd9c8b0464fe98da646bf
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-04-14 11:16:20 +0900

    0.5.9
    sec filter

--- spring-webmvc/src/main/kotlin/io/github/hchanjune/omk/webmvc/config/SecurityFilterConfiguration.kt
+++ spring-webmvc/src/main/kotlin/io/github/hchanjune/omk/webmvc/config/SecurityFilterConfiguration.kt
@@ -2,6 +2,7 @@ package io.github.hchanjune.omk.webmvc.config
 
 import io.github.hchanjune.omk.core.provider.IssuerProvider
 import io.github.hchanjune.omk.webmvc.filter.SpringSecurityConfigurationFilter
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
@@ -18,10 +19,14 @@ class SecurityFilterConfiguration(
     private val issuerProvider: IssuerProvider
 ) {
 
+    private val logger = LoggerFactory.getLogger(SecurityFilterConfiguration::class.java)
+
     @Bean
     fun securityIssuerInjector(): BeanPostProcessor = object : BeanPostProcessor {
         override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+            logger.info (">>> BeanPostProcessor: ${bean::class.simpleName} ($beanName)")
             if (bean is HttpSecurity) {
+                logger.info((">>> HttpSecurity caught!"))
                 bean.addFilterAfter(
                     SpringSecurityConfigurationFilter(issuerProvider),
                     AuthorizationFilter::class.java
